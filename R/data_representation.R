d3b5f375ebdc66674e97566a033d465c041b3746
Yi-Jiang <jiangyi3029@foxmail.com>
2022-10-08 22:46:29 +0800

    filter

--- R/data_representation.R
+++ R/data_representation.R
@@ -164,9 +164,9 @@ format_genotypes <- function(genotypes, vcf=FALSE, vcfName, R2_cutoff_up=1.1, R2
   filter_AF <- sapply(AF, function(x) filter_by_AF(x, MAF_cutoff))
   filter_R2 <- sapply(R2, function(x) filter_by_R2(x, R2_cutoff_up, R2_cutoff_down))
   filter_HWE <- sapply(hwe_p, function(x) filter_by_HWE(x, 1e-6))
-  filter <- paste(filter_AF, filter_R2, filter_HWE, sep=",")
-  filter[filter=="PASS,PASS,PASS"] <- "PASS"
-  filter[filter!="PASS,PASS,PASS"] <- gsub(",PASS", "", gsub("PASS,", "", filter[filter!="PASS,PASS,PASS"]))
+  filter <- paste(filter_AF, filter_R2, filter_HWE, sep=";")
+  filter[filter=="PASS;PASS;PASS"] <- "PASS"
+  filter[filter!="PASS;PASS;PASS"] <- gsub(";PASS", "", gsub("PASS;", "", filter[filter!="PASS;PASS;PASS"]))
   
   ## Mark probes failed the first iteration in callGeno.
   AF <- round(AF, 3)
