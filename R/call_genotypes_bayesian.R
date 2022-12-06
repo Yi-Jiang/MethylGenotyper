1d7abee02035f2d7d13d207311a819c2e2067e6d
Yi-Jiang <jiangyi3029@foxmail.com>
2022-12-06 09:17:41 +0800

    EM

--- R/call_genotypes_bayesian.R
+++ R/call_genotypes_bayesian.R
@@ -19,8 +19,8 @@ call_genotypes_bayesian <- function(RAI, pop, type, maxiter=50){
   assignments <- RAI %>% as.data.frame %>%
     mutate(Probe=rownames(.)) %>%
     tidyr::gather(key="Sample", value="RAI", -Probe) %>% tibble
-  if(nrow(assignments) > 200000){
-    assignments <- assignments[sample(1:nrow(assignments), 200000),]
+  if(nrow(assignments) > 100000){
+    assignments <- assignments[sample(1:nrow(assignments), 100000),]
   }
   N <- nrow(assignments)
   
