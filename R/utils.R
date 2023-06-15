1195bbed233b449644a145755f2ed10619b6c34b
Yi-Jiang <jiangyi3029@foxmail.com>
2023-06-15 10:32:20 +0800

    info

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,18 @@
 .onAttach <- function(libname, pkgname) {
   message("Loading package: ", pkgname)
   message("Located in library: ", libname)
-  packageStartupMessage("Welcome to my package")
+  #packageStartupMessage("Welcome to my package")
+  
+  suppressMessages({
+    library(minfi)
+    library(tidyverse)
+    library(foreach)
+    library(doParallel)
+    library(HardyWeinberg)
+    library(multimode)
+    library(rlist)
+    library(stats4)
+    library(ggplot2)
+    library(ggpubr)
+  })
 }
