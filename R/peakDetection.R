17deff91d200356a0949e12b691f7fa4c789d163
Yi-Jiang <jiangyi3029@foxmail.com>
2023-10-26 11:20:27 +0800

    add density of mode test

--- R/peakDetection.R
+++ R/peakDetection.R
@@ -18,7 +18,7 @@ getMod <- function(x, bw=0.04, minDens=0.001, cpu=1){
       loc <- locmodes(x[cpg,], mod0=nMod)
       if(length(loc$locations) < 2){
         print(paste0("Escape ", cpg, " as <2 valid peaks detected."))
-        return(c(CpG=cpg, nmod=length(loc$locations), loc_pass=FALSE, loc0=NA, loc1=NA, loc2=NA))
+        return(c(CpG=cpg, nmod=length(loc$locations), loc_pass=FALSE, loc0=NA, loc1=NA, loc2=NA, dens0=NA, dens1=NA, dens2=NA))
       }
       idx <- seq(1, length(loc$locations), 2)
       idx2 <- seq(2, length(loc$locations), 2)
@@ -30,12 +30,17 @@ getMod <- function(x, bw=0.04, minDens=0.001, cpu=1){
       modes <- modes[!lowDens,]
       antimodes <- antimodes[!lowDens[-1],]
       if(lowDens[1]){antimodes <- antimodes[-1,]}
+      modes <- modes[order(modes$loc),]
+      antimodes <- antimodes[order(antimodes$loc),]
 
       # Detect the central mode
       if(nrow(modes)==2){
-        loc012 <- findCentralFromTwoPeaks(modes$loc)
-      }else if(nrow(modes)==3){
-        loc012 <- sort(modes$loc)
+        distance <- abs(modes$loc - 0.5)
+        if(distance[1] < distance[2]){
+          modes <- rbind(NA, modes)
+        }else{
+          modes <- rbind(modes, NA)
+        }
       }else if(nrow(modes)>3){ # remove the lowest peaks as they are mostly noise
         while(nrow(modes)>3){
           lowest <- which.min(modes$dens)
@@ -46,17 +51,23 @@ getMod <- function(x, bw=0.04, minDens=0.001, cpu=1){
           }
           modes <- modes[-lowest,]
         }
-        loc012 <- sort(modes$loc)
-      }else{
+      }else if(nrow(modes)<2){
         print(paste0("Escape ", cpg, " as <2 valid peaks detected."))
-        return(c(CpG=cpg, nmod=nrow(modes), loc_pass=FALSE, loc0=NA, loc1=NA, loc2=NA))
+        return(c(CpG=cpg, nmod=nrow(modes), loc_pass=FALSE, loc0=NA, loc1=NA, loc2=NA, dens0=NA, dens1=NA, dens2=NA))
       }
+      loc012 <- modes$loc
+      dens012 <- modes$dens
+      
+      # Position of the central mode in a given range?
       if(loc012[2]>0.3 & loc012[2]<0.7){
-        #if(all(c(loc012[1]<.3, loc012[3]>.7), na.rm=TRUE)){
           loc_pass=TRUE
-        #}else{loc_pass=FALSE}
-      }else{loc_pass=FALSE}
-      return(c(CpG=cpg, nmod=nrow(modes), loc_pass=loc_pass, loc0=loc012[1], loc1=loc012[2], loc2=loc012[3]))
+      }else{
+        loc_pass=FALSE
+      }
+      
+      return(c(CpG=cpg, nmod=nrow(modes), loc_pass=loc_pass, 
+               loc0=loc012[1], loc1=loc012[2], loc2=loc012[3], 
+               dens0=dens012[1], dens1=dens012[2], dens2=dens012[3]))
     }, error = function(e) return(paste0("Escape ", cpg, " with error: ", e)))
   }
   stopImplicitCluster()
@@ -66,28 +77,9 @@ getMod <- function(x, bw=0.04, minDens=0.001, cpu=1){
   modRes$loc0 <- as.numeric(as.character(modRes$loc0))
   modRes$loc1 <- as.numeric(as.character(modRes$loc1))
   modRes$loc2 <- as.numeric(as.character(modRes$loc2))
+  modRes$dens0 <- as.numeric(as.character(modRes$dens0))
+  modRes$dens1 <- as.numeric(as.character(modRes$dens1))
+  modRes$dens2 <- as.numeric(as.character(modRes$dens2))
   rownames(modRes) <- modRes$CpG
   modRes
 }
-
-#' Find the central peak from a distribution of two peaks
-#' 
-#' @param locations A vector of two, indicating the locations of the two peaks.
-#' @return A vector of three, with the central one being the location of the central peak.
-#' @export
-findCentralFromTwoPeaks <- function(locations){
-  if(length(locations)!=2){
-    print("Error: findCentralFromTwoPeaks() only accept a vector of two as input.")
-    return(NA)
-  }
-  locations <- c(NA, locations, NA)
-  distance <- abs(locations - 0.5)
-  if(distance[2] < distance[3]){
-    loc012 <- locations[1:3]
-  }else{
-    loc012 <- locations[2:4]
-  }
-  loc012
-}
-
-
